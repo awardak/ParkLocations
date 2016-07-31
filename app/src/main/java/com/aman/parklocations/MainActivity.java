f016ff5a1b3a5c988c4e87dd1071d364f9da8b44
awardak <amanwardak@gmail.com>
2016-07-30 19:20:36 -0700

    use GoogleApiClient to get current location

--- app/src/main/java/com/aman/parklocations/MainActivity.java
+++ app/src/main/java/com/aman/parklocations/MainActivity.java
@@ -1,13 +1,91 @@
 package com.aman.parklocations;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener{
+    private final String TAG = "MainAct";
+    private GoogleApiClient googleApiClient;
+    private final int PERMISSION_CODE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // build GoogleApiClient to use LocationServices API
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    /* this callback is called when GoogleApiClient is successfully connected */
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // if we don't have location permission, request it
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_CODE);
+            Log.v(TAG, "requesting permissions");
+
+        } else {
+            startLocationUpdates();
+        }
+    }
+
+    /* this callback is called after the user is asked for location permissions */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationUpdates();
+                } else {
+                    Log.v(TAG, "permission denied by user");
+                }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.v(TAG, "onLocationChanged() called - lat = " + location.getLatitude() + " lon = " + location.getLongitude());
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void startLocationUpdates() {
+        Log.v(TAG, "startLocationUpdates() called");
+
+        try {
+            LocationRequest locationRequest = LocationRequest.create().setPriority(LocationRequest.PRIORITY_LOW_POWER);
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+        } catch (SecurityException exc) {
+            Log.v(TAG, "location security exception");
+        }
     }
 }
